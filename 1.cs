de8dad3bf7496668ce0c3c6cac97a1d4ad8b7745
Mdr11228 <Mohammaddarbandi.85@gmail.com>
2025-10-17 23:57:35 +0330

    Add files via upload

---
+++ 1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the radius: ");
+        double radius = Convert.ToDouble(Console.ReadLine());
+
+        double circumference = 2 * Math.PI * radius;
+        double area = Math.PI * Math.Pow(radius, 2);
+
+        Console.WriteLine("masahat: " + circumference);
+        Console.WriteLine("mohit: " + area);
+        Console.ReadKey();
+    }
+}
---
+++ 2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double a,d;
+            Console.Write("Enter a temp : ");
+            a = Double.Parse(Console.ReadLine());
+            d = a * 1.8 + 32;
+            Console.WriteLine(d);
+            Console.ReadKey();
+        }
+    }
+}
