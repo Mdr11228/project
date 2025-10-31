e79a9a941f8ddc7678bc07983c0759d19cd5ff5b
Mdr11228 <Mohammaddarbandi.85@gmail.com>
2025-10-31 19:07:32 +0330

    Add files via upload

---
+++ 3_1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] day = new string[] { "Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday" };
+            Console.Write("enter number: ");
+            int a = int.Parse(Console.ReadLine());
+            if (a >= 1 && a <= 7)
+            {
+                Console.WriteLine(day[a - 1]);
+            }
+            else
+            {
+                Console.WriteLine("end!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
